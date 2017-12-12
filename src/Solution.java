5d940aa60dde1b739ebea21baff022c1dc3d8d23
Rui <rui@dhcp78.merunorth109.iit.edu>
2017-12-11 22:57:02 -0600

    first commit

--- src/Solution.java
+++ src/Solution.java
@@ -1,4 +1,20 @@
 
 public class Solution {
+	public String intToRoman(int num){
+		int[] number = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
 
+		String[] roman = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+		String ans = "";
+		int i = 0;
+		
+		while(num != 0){
+			if(num >= number[i]){
+				num -= number[i];
+				ans += roman[i];
+			}else{
+				i ++;
+			}
+		}
+		return ans;
+	}
 }
